e458095891d681f66a66068d05576c7942186f92
D.S <64180264+flstudio4@users.noreply.github.com>
2022-09-23 22:02:01 -0500

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+"use strict";
+
+/*
+    Project: Final project
+    Author: Dmitrii Sumenko
+    Date: 9/23/2022
+    file name: script.js
+
+ */
+
+    function compute() {
+        let principal = document.getElementById("principal").value;
+        principal = parseInt(principal);
+
+        if (principal <= 0) {
+            alert("Enter a positive number");
+            document.getElementById("principal").value = "";
+            document.getElementById("principal").focus();
+        }
+
+        else if (principal > 0) {
+            let rate = document.getElementById("rate").value;
+            let years = document.getElementById("years").value;
+            let interest = principal * years * rate / 100;
+            let year = new Date().getFullYear() + parseInt(years);
+
+            document.getElementById("result").innerHTML = `<p id="text">If you deposit <mark>${principal}</mark>,<br>at an interest
+                                                                   rate of <mark>${rate}%</mark>.<br>You will receive an amount
+                                                                   of <mark>${interest}</mark>,<br>in the year <mark>${year}</mark></p>`;
+        }
+    }
+
+    function updateRate() {
+        document.getElementById("rate_val").innerText=document.getElementById("rate").value + "%";
+    }
         
