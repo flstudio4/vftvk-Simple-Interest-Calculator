b354764f70b73e0e61fcc5415a23e3f8458f9c83
D.S <64180264+flstudio4@users.noreply.github.com>
2022-09-23 22:17:19 -0500

    Added comments

--- script.js
+++ script.js
@@ -10,26 +10,29 @@
 
     function compute() {
         let principal = document.getElementById("principal").value;
-        principal = parseInt(principal);
 
+        // Validates principal input field for positive numbers only
         if (principal <= 0) {
             alert("Enter a positive number");
             document.getElementById("principal").value = "";
             document.getElementById("principal").focus();
         }
 
+        // If input is valid, proceed to calculations
         else if (principal > 0) {
             let rate = document.getElementById("rate").value;
             let years = document.getElementById("years").value;
             let interest = principal * years * rate / 100;
             let year = new Date().getFullYear() + parseInt(years);
 
+            // compose inner HTML for displaying results of calculations
             document.getElementById("result").innerHTML = `<p id="text">If you deposit <mark>${principal}</mark>,<br>at an interest
                                                                    rate of <mark>${rate}%</mark>.<br>You will receive an amount
                                                                    of <mark>${interest}</mark>,<br>in the year <mark>${year}</mark></p>`;
         }
     }
 
+    // Update number of percent for slider
     function updateRate() {
         document.getElementById("rate_val").innerText=document.getElementById("rate").value + "%";
     }
